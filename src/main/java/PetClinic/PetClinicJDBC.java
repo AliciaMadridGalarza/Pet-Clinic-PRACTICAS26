a10ebe9de5987b8b3544c6463ec6f8b318ba5360
alicia.madrid <alimadgal@alu.edu.gva.es>
2026-05-21 12:15:56 +0200

    commit PETCLINIC JDBC

--- src/main/java/PetClinic/PetClinicJDBC.java
+++ src/main/java/PetClinic/PetClinicJDBC.java
@@ -1,4 +1,29 @@
 package PetClinic;
 
+
+import java.sql.*;
+
 public class PetClinicJDBC {
+    public static void main(String[] args){
+        String url = "jdbc:mysql://localhost:3306/PETCLINIC?serverTimezone=UTC";
+        String username = "root";
+        String password  = "ROOT_ALICIA_2802";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username,password);
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM OWNERS");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("id") + " | "+ resultSet.getString("first_name ")+ " | "+resultSet.getString("last_name "));
+            }
+            connection.close();
+            statement.close();
+            resultSet.close();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
